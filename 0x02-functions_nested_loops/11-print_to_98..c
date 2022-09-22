783669d11e4386589e85828a77eb7a455998b1c0
Jas_salama <yasminesalama2@gmail.com>
2022-09-23 01:06:54 +0200

    0x02

--- 0x02-functions_nested_loops/11-print_to_98..c
+++ 0x02-functions_nested_loops/11-print_to_98..c
@@ -1,20 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-* print_to_98 - prints all natural numbers from n to 98
-*@n: starting point
+* print_to_98 - Prints all natural numbers from input to 98,
+*               in order separated by a comma followed by a space.
+* @n: The number to begin counting at.
 */
-
 void print_to_98(int n)
 {
-if (n <= 98)
+if (n >= 98)
 {
-for (n = n; n <= 97; n++)
-printf("%d, ", n);
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
 }
+
 else
-for (n = n; n > 98; n--)
-printf("%d, ", n);
-printf("98\n");
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
-#include "main .h"
+#include "holberton.h"
 
 /**
-* print_last_digit - Prints the last digit of a number.
-* @n: The number in question.
-*
-* Return: Value of the last digit.
+* print_last_digit - prints the last digit of a number
+*@n: number being tested
+* Return: Always 0.
 */
+
 int print_last_digit(int n)
 {
-int last_digit = n % 10;
+n = n % 10;
 
-_putchar(last_digit);
-
-return (last_digit);
+if (n < 0)
+n = -n;
+_putchar(n + '0');
+return (n);
 }
