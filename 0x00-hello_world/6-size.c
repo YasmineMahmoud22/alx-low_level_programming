480206a2d66321acb0e06d97046c5ad6fb7832ca
Jas_salama <yasminesalama2@gmail.com>
2022-12-10 13:42:37 +0200

    TASK6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int LONGINT;
 long long int LG;
 float FL;
 
-printf("Size of a char: %zu byte\n",sizeof(CHARTYPE));
-printf("Size of an int: %zu byte(s)\n",sizeof(INTTYPE));
-printf("Size of a long int: %zu byte(s)\n",sizeof(LONGINT));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(LG));
-printf("Size of a float: %zu byte(s)\n",sizeof(FL));
+printf("Size of a char: %zu byte\n", sizeof(CHARTYPE));
+printf("Size of an int: %zu byte(s)\n", sizeof(INTTYPE));
+printf("Size of a long int: %zu byte(s)\n", sizeof(LONGINT));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(LG));
+printf("Size of a float: %zu byte(s)\n", sizeof(FL));
 return (0);
 }
