4f3a5dc3ec2b3d1ef26452be3c03badacb4cd155
Jas_salama <yasminesalama2@gmail.com>
2022-12-20 21:16:50 +0200

    pointers&arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,9 @@ void print_rev(char *s)
 {
 int i;
 
-for (i = 0; s[i] != '\0'; i++);
-for (i = i-1; s[i] != '\0'; i--)
+for (i = 0; s[i] != '\0'; i++)
+    ;
+for (i = i - 1; s[i] != '\0'; i--)
 {
 _putchar(s[i]);
 }
