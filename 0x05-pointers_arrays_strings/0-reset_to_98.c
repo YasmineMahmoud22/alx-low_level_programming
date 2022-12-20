59a0d32d20889f2654f1d9d36d187f77841df381
Jas_salama <yasminesalama2@gmail.com>
2022-12-20 15:10:04 +0200

    poniters&arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 
 
 /**
-*modif_my_param - set the integer to 98
+* 0-reset_to_98 - reset the integer to 98
 * @m: a pointer the integer we want to set to 98
 *
 * Return: nothing
 */
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+ {
 *n = 98 ;
 
 }
