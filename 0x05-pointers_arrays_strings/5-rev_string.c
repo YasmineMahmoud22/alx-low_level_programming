e4bfd1d2458179293feddc22bbd3e0bb93f27b96
Jas_salama <yasminesalama2@gmail.com>
2022-12-20 22:49:46 +0200

    pointers&arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,12 @@
 void rev_string(char *s)
 {
 char word;
-int i, length1 ,length2;
+int i ,length1 ,length2;
 length1 = 0;
 length2 = 0;
 
 while(s[length1] != '\0')
-{
 length1++;
-}
 length2 = length1 - 1;
 for(i = 0; i < length1; i++)
 {
