9aed6b9a2aa422aab4356c3f545e190a1eeaf139
Jas_salama <yasminesalama2@gmail.com>
2022-12-20 20:19:48 +0200

    pointers&arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,11 @@
 
 void _puts(char *str)
 {
+	int index;
 
-	for (; str != '\0'; str++)
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(str);
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
