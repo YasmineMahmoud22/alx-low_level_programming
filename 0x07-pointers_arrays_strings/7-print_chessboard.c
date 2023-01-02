cba9d725dff8ae3a7c0d900bc38cd36ba9ea6bf0
Jas_salama <yasminesalama2@gmail.com>
2023-01-02 22:38:09 +0200

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,16 @@
 */
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+int i = 0;
+int j;
 
-for (i = 0; i <= 7; i++)
+while (i < 8)
 {
-for (j = 0; j <= 7; j++)
+for (j = 0; j < 8; j++)
 {
 _putchar(a[i][j]);
 }
 _putchar('\n');
+i++;
 }
 }
